5c36b0e2c74a5e16d425609ce4f50da9cf076e4b
fadhilahumairah <fadhilahumairah24@gmail.com>
2023-03-01 01:07:07 +0700

    add tailwind to react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import "./App.css";
+import React from "react";
 import HelloWorld from "./Components/HelloWorld";
-import CounterExample from "./Components/CounerExample";
+import CounterExample from "./Components/CounterExample";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div>
-      {/* <HelloWorld name="Dekusssrr" /> */}
-      <CounterExample />
+      <Header />
+      <HelloWorld name="Fala" />
+      {/* <CounterExample /> */}
+      <Footer />
     </div>
   );
 }
