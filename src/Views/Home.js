9b387b8073f17c03e10a376f70d5dfcaddfe3af6
fadhilahumairah <fadhilahumairah24@gmail.com>
2023-03-01 11:45:55 +0700

    custom react hooks

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,25 +1,13 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Loader from "../Components/Loader";
 import HelloWorld from "../Components/HelloWorld";
 import ProductCard from "../Components/ProductCard";
+import { useAxiosGet } from "../Hooks/HttpRequest";
 
 function Home() {
   const url = `https://63feb06f370fe830d9d744f9.mockapi.io/learning-react/api/product?page=1&limit=10`;
-  const [product, setProduct] = useState({ loading: false, data: null });
+  let product = useAxiosGet(url);
   let content = null;
-
-  useEffect(() => {
-    setProduct({ loading: true, data: null, error: false });
-    axios
-      .get(url)
-      .then((response) => {
-        setProduct({ loading: false, data: response.data, error: false });
-      })
-      .catch(() => {
-        setProduct({ loading: false, data: null, error: true });
-      });
-  }, [url]);
 
   if (product.loading) {
     content = <Loader />;
--- src/Views/Product.js
+++ src/Views/Product.js
@@ -2,25 +2,15 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import Loader from "../Components/Loader";
+import { useAxiosGet } from "../Hooks/HttpRequest";
 
 function Product() {
   const { id } = useParams();
   const url = `https://63feb06f370fe830d9d744f9.mockapi.io/learning-react/api/product/${id}`;
-  const [product, setProduct] = useState({ loading: false, data: null });
+
+  let product = useAxiosGet(url);
 
   let content = null;
-
-  useEffect(() => {
-    setProduct({ loading: true, data: null, error: false });
-    axios
-      .get(url)
-      .then((response) => {
-        setProduct({ loading: false, data: response.data, error: false });
-      })
-      .catch(() => {
-        setProduct({ loading: false, data: null, error: true });
-      });
-  }, [url]);
 
   if (product.loading) {
     content = <Loader />;
